ff42292bf189134b8e18d19afefe9e29c61e1062
MartaKwiatek <66514449+MartaKwiatek@users.noreply.github.com>
2021-06-15 18:36:24 +0200

    jacoco plugin added (#2)
    * jacoco plugin added
    new github action pipeline created
    
    * Lombok set up
    Basic structure for invoicing system created
    Unit and integration tests added
    
    * gradlew permissions
    
    * main.yml steps deleted

---
+++ app/src/main/java/pl/futurecollars/invoicing/model/Vat.java
@@ -0,0 +1,19 @@
+package pl.futurecollars.invoicing.model;
+
+import java.math.BigDecimal;
+import lombok.Getter;
+
+@Getter
+public enum Vat {
+
+    VAT_0(0),
+    VAT_5(5),
+    VAT_8(8),
+    VAT_23(23);
+
+    private final BigDecimal rate;
+
+    Vat(int rate) {
+        this.rate = BigDecimal.valueOf(rate);
+    }
+}
