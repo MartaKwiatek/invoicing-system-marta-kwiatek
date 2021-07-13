d3105f5f296646b155d6029d77355f5ea9d9a291
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-07-13 13:16:53 +0200

    API documentation with Swagger added

--- app/src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,5 +1,6 @@
 package pl.futurecollars.invoicing.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Company {
 
+    @ApiModelProperty(value = "Tax identification number", required = true, example = "666-666-69-69")
     private String taxIdNumber;
+
+    @ApiModelProperty(value = "Company address", required = true, example = "ul. Żwirki i Wigury 80, 87-100 Toruń")
     private String address;
+
+    @ApiModelProperty(value = "Name of Company", required = true, example = "Radio Szatan. Głos szatana w Twoim domu xD")
     private String name;
 }
