7293daec37608eb7267efeb27055556a47fd7227
MartaKwiatek <66514449+MartaKwiatek@users.noreply.github.com>
2021-09-03 09:40:30 +0200

    frontend implemented (#16)

---
+++ src/main/resources/public/company.js
@@ -0,0 +1,57 @@
+const loadCompanies = async () => {
+    const response = await fetch('http://localhost:7777/companies');
+    const companies = await response.json();
+
+    const companiesTable = document.getElementById("companiesTable");
+    companies.forEach(company => {
+        const row = companiesTable.insertRow(-1);
+
+        const tinCell = row.insertCell(0);
+        tinCell.innerText = company.taxIdNumber;
+
+        const addressCell = row.insertCell(1);
+        addressCell.innerText = company.address;
+        
+        const nameCell = row.insertCell(2);
+        nameCell.innerText = company.name;
+
+        const healthInsuranceCell = row.insertCell(3);
+        healthInsuranceCell.innerText = company.healthInsurance;
+
+        const pensionInsuranceCell = row.insertCell(4);
+        pensionInsuranceCell.innerText = company.pensionInsurance;
+    });
+}
+
+const serializeFormToJson = form => JSON.stringify(
+    Array.from(new FormData(form).entries())
+        .reduce((m, [key, value]) =>
+            Object.assign(m, {[key]: value}), {})
+);
+
+function handleAddCompanyFormSubmit() {
+    const form = $("#addCompanyForm");
+    form.on('submit', function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: 'companies',
+            type: 'post',
+            contentType: 'application/json',
+            data: serializeFormToJson(this),
+            success: function (data) {
+                $("#companiesTable").find("tr:gt(0)").remove();
+                alert(data)
+                loadCompanies()
+            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                alert(errorThrown)
+            }
+        });
+    });
+}
+
+window.onload = function () {
+    loadCompanies();
+    handleAddCompanyFormSubmit()
+};
