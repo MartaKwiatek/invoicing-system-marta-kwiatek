7a13c7b7c66efafeb9a56349741aeb5825961893
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-09-03 14:10:12 +0200

    add company feature added bootstrap added

--- angular-frontend/invoicing-app/src/app/app.component.ts
+++ angular-frontend/invoicing-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Company } from './model/company';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'invoicing-app';
+  title = 'Invoicing Application';
+
+  companies: Company[] = [
+    new Company(
+      "111-111-11-11",
+      "ul. First 1",
+      "First Company Ltd.",
+      111.11,
+      111.11
+    ),
+    new Company(
+      "222-222-22-22",
+      "ul. Second 2",
+      "Second Company Ltd.",
+      222.22,
+      222.22
+    )
+  ];
+
+  newCompany = new Company("", "", "", 0, 0);
+
+  addCompany() {
+    this.companies.push(this.newCompany);
+    this.newCompany = new Company("", "", "", 0, 0);
+  }
 }
---
+++ angular-frontend/invoicing-app/src/app/model/company.ts
@@ -0,0 +1,13 @@
+export class Company {
+
+  constructor(
+    public taxIdNumber: string,
+    public address: string,
+    public name: string,
+    public healthInsurance: number,
+    public pensionInsurance: number
+  ) {
+    
+  }
+
+}
