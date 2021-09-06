bcb6ddcd91d00626a8239ffe993f71217be040fb
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-09-06 23:28:38 +0200

    MockedCompanyService added bootstrap in karma.conf.js added

--- angular-frontend/invoicing-app/src/app/app.component.spec.ts
+++ angular-frontend/invoicing-app/src/app/app.component.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
+import { Observable, of } from 'rxjs';
 import { AppComponent } from './app.component';
+import { CompanyService } from './company.service';
+import { Company } from './model/company';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        { provide: CompanyService, useClass: MockCompanyService }
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +22,43 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'invoicing-app'`, () => {
+  it(`should have as title 'Invoicing Application'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('invoicing-app');
+    expect(app.title).toEqual('Invoicing Application');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('invoicing-app app is running!');
+    expect(compiled.querySelector('div.greeting').textContent).toContain('Hello invoicing app!');
   });
+
+class MockCompanyService {
+
+  companies: Company[] = [
+    new Company(
+      1,
+      "111-111-11-11",
+      "ul. First 1",
+      "First Company Ltd",
+      1111.11,
+      999.99
+    ),
+    new Company(
+      2,
+      "222-222-22-22",
+      "ul. Second 2",
+      "Second Company Ltd",
+      2222.22,
+      1212.12
+    )
+  ];
+
+  getCompanies() {
+    return of(this.companies);
+  }
+}
+
 });
