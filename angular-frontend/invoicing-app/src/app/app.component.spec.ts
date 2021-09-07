6631686d07d92573d96d0a91eef55a4e1a7708ac
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-09-07 10:47:02 +0200

    company service tests added

--- angular-frontend/invoicing-app/src/app/app.component.spec.ts
+++ angular-frontend/invoicing-app/src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { Observable, of } from 'rxjs';
 import { AppComponent } from './app.component';
 import { CompanyService } from './company.service';
-import { Company } from './model/company';
+import { Company } from './company';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -35,6 +35,20 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('div.greeting').textContent).toContain('Hello invoicing app!');
   });
 
+  it('should display companies', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.ngOnInit();
+    fixture.detectChanges();
+    const mainPage = fixture.nativeElement;
+    expect(app.companies.length).toBe(2);
+    expect(mainPage.textContent).toContain('222-222-22-22ul. Second 2Second Ltd.2222.22222.22');
+
+    const createCompanyButton = mainPage.querySelector("#createCompanyButton");
+    createCompanyButton.click();
+    expect(app.companies.length).toBe(3);
+  });
+
 class MockCompanyService {
 
   companies: Company[] = [
@@ -42,23 +56,28 @@ class MockCompanyService {
       1,
       "111-111-11-11",
       "ul. First 1",
-      "First Company Ltd",
+      "First Ltd.",
       1111.11,
-      999.99
+      111.11
     ),
     new Company(
       2,
       "222-222-22-22",
       "ul. Second 2",
-      "Second Company Ltd",
+      "Second Ltd.",
       2222.22,
-      1212.12
+      222.22
     )
   ];
 
   getCompanies() {
     return of(this.companies);
   }
+
+  addCompany(company: Company): Observable<any> {
+    this.companies.push(company);
+    return of();
+}
 }
 
 });
