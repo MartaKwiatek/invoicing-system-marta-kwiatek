1441a03d2dfd1ef5ad8e99953d7cb1d708e7f524
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-09-09 13:04:12 +0200

    done

--- src/main/java/pl/futurecollars/invoicing/config/WebSecurityConfig.java
+++ src/main/java/pl/futurecollars/invoicing/config/WebSecurityConfig.java
@@ -21,7 +21,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
         http.httpBasic()
                 .and()
                 .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
